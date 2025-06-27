39d2d87260a1dbcd50fa083d9fb6979b9e46c69a
George Georgiev <georgegi86@gmail.com>
2025-06-27 09:49:30 -0500

    Update ic_analysis.py

--- src/alphalytics/ic_analysis.py
+++ src/alphalytics/ic_analysis.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 import numpy as np
-from scipy.stats import spearmanr, stats, t, ttest_1samp
+from scipy.stats import spearmanr, wilcoxon, binomtest, t, ttest_1samp, skew, kurtosis
 
 
 # ============== INFORMATION COEFFICIENT ANALYSIS ============== #
