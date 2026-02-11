30256e2e8093f16ea0023c0636638689515177d1
George Georgiev <georgegi86@gmail.com>
2026-02-11 12:10:12 -0600

    Update __init__.py

--- src/alphalytics/__init__.py
+++ src/alphalytics/__init__.py
@@ -17,13 +17,18 @@ finally:
 
 
 from .ic_analysis import cs_spearmanr, ts_spearmanr, compute_ic_stats, factor_decay
+
 from .quantile_analysis import to_quantiles, compute_quantile_returns, get_quantile_holdings, \
     compute_mean_quantile_forward_return, fwd_quantile_stats
+
 from .turnover_analysis import calculate_autocorrelation, compute_factor_autocorr, compute_quantile_turnover, compute_quantiles_turnover
+
 from .performance_metrics import cumgrowth, compute_performance_table, compute_cumulative_growth, \
     compute_forward_returns, compute_capm, down_capture, up_capture
+
 from .plotting import plot_factor_data, plot_cumulative_performance, plot_quantiles_risk_metrics, plot_quantile_correlations, plot_spearman_rank, \
     plot_ic_hist, qqplot_ic, plot_ic_summary, plot_factor_decay, plot_forward_returns, plot_quantiles_annual_turnover
+
 from .utils import detect_extreme_outliers, detect_internal_nan, fill_first_nan
 
 __all__ = [
