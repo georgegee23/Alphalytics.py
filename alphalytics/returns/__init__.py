509f5507c07a4e2f720f40c1237df6b7b78b28aa
George Georgiev <georgegi86@gmail.com>
2026-03-18 15:03:22 -0500

    Update __init__.py

--- alphalytics/returns/__init__.py
+++ alphalytics/returns/__init__.py
@@ -8,7 +8,7 @@ from .risk import downside_variance, annual_std, sortino_ratio
 from .capm import compute_capm, beta, bull_bear_beta, rolling_beta
 
 from .relative import (
-    active_return, tracking_error, information_ratio,
+    active_return, tracking_error, information_ratio, rolling_information_ratio,
     batting_average, bull_batting_average, bear_batting_average,
     batting_averages,
     win_loss_ratio, bull_win_loss_ratio, bear_win_loss_ratio,
